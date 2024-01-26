9a28cb19e43157f4bc5bea4b0a02fa90c8b38f56
pengzha0 <2570288754@qq.com>
2024-01-26 09:17:29 +0800

    first commit

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+# from .vavanilla_resnet_cifar import vanilla_resnet20
+from .vanilla_models.vanilla_resnet_cifar import vanilla_resnet20
+from .vanilla_models.vanilla_resnet_imagenet import resnet18
+
+
+from .quan_resnet_imagenet import resnet18_quan, resnet34_quan
+
+from .quan_alexnet_imagenet import alexnet_quan
+
+from .quan_mobilenet_imagenet import mobilenet_v2_quan
+from .vanilla_models.vanilla_mobilenet_imagenet import mobilenet_v2
+
+from .quan_resnet_cifar10 import resnet20_quan
